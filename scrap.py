34850a47f2ca429ae9c78acc0053d60f6c79109c
Bissapinho <alexandregalle3@gmail.com>
2025-10-08 00:13:48 +0200

    message: [main 0000000] Initial commit

---
+++ scrap.py
@@ -0,0 +1,28 @@
+import requests
+
+def run_query(query):
+    headers = {"Content-Type": "application/json"}
+    response = requests.post('https://api.tarkov.dev/graphql', headers=headers, json={'query': query})
+    if response.status_code == 200:
+        return response.json()
+    else:
+        raise Exception("Query failed to run by returning code of {}. {}".format(response.status_code, query))
+
+
+new_query = """
+{
+  
+  maps(lang: en, name: "Customs") {
+    artillery {
+      zones {
+        top
+        radius
+        bottom
+      }
+    }
+  }
+}
+"""
+
+result = run_query(new_query)
+print(result)
