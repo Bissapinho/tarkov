818690fefe3cc2e8ea4d8802bf6918b574c6cf13
Bissapinho <alexandregalle3@gmail.com>
2025-10-08 11:40:21 +0200

    test value_per_sq metric

--- scrap.py
+++ scrap.py
@@ -1,28 +1,67 @@
 import requests
 
-def run_query(query):
+
+items = ["AS VAL 9x39 special assault rifle Default",
+         "ASh-12 12.7x55 assault rifle Default",
+         "B&T MP9 9x19 submachine gun Default",
+         "B&T MP9-N 9x19 submachine gun Default",
+         "20x1mm toy gun Default"]
+
+
+def run_query():
+    
+    global items #temporary, use tkinter interface later
+    responses = []
     headers = {"Content-Type": "application/json"}
-    response = requests.post('https://api.tarkov.dev/graphql', headers=headers, json={'query': query})
-    if response.status_code == 200:
-        return response.json()
-    else:
-        raise Exception("Query failed to run by returning code of {}. {}".format(response.status_code, query))
+
+    for item in items:
+        new_query = f"""
+    {{
+      items(lang: en, name: "{item}") {{
+        id
+        name
+        avg24hPrice
+        basePrice
+        low24hPrice
+        width
+        imageLink
+        lastOfferCount
+        height
+      }}
+    }}
+    """    
+        response = requests.post('https://api.tarkov.dev/graphql', headers=headers, json={'query': new_query})
+        if response.status_code == 200:
+            responses.append(response.json())
+        else:
+            continue
+    return responses
 
 
-new_query = """
-{
-  
-  maps(lang: en, name: "Customs") {
-    artillery {
-      zones {
-        top
-        radius
-        bottom
-      }
-    }
-  }
-}
-"""
-
-result = run_query(new_query)
+result = run_query()
 print(result)
+
+def parse_result(result):
+    info = {}
+    for r in result:
+        local_info = {}
+        items = r.get("data", {}).get("items", [])
+        if not items:
+            continue 
+
+        data = items[0]
+        size = float(data['width']) * float(data['height']) or 1 #will prevent division by 0 if any pb occur
+        avg_price = data['avg24hPrice']
+        value_per_sq = avg_price / size
+
+        #Add info of the iterated item to the locl dict
+
+        local_info = {
+    "value per squares": value_per_sq,
+    "Image": data["imageLink"]
+    }
+
+        info[data['name']] = local_info #adding to the global dict the info about the local item
+    return info
+
+print(parse_result(result))
